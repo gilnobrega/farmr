eaf1bd423b5c2e1c338bd540585aa71a8baac3e9
Joaquim <jgilfnuk@gmail.com>
2021-04-12 21:15:35 +0100

    hopefully fixed it this time

--- run.sh
+++ run.sh
@@ -1,20 +1,31 @@
 #!/bin/sh
-if [ ! -f "/usr/bin/dart" ] || [ ! -f "/usr/bin/screen" ] || [ ! -f "/usr/bin/git"]; then
+
+#Installs dart
+if [ ! -f "/usr/bin/dart" ]; then
 
     echo "Installing dependencies (dart, git, screen)";
     sleep 2 ;
     wget https://storage.googleapis.com/dart-archive/channels/stable/release/latest/linux_packages/dart_2.12.2-1_amd64.deb -O dart.deb;
     sudo dpkg -i dart.deb;
-    sudo apt-get install git screen -y;
-    sudo apt-get install -f;
     rm dart.deb;
+    sudo apt-get install -f;
 
-    git init;
-    git remote add origin https://github.com/joaquimguimaraes/chiabot.git;
-
-    dart pub get;
 fi
 
+#Installs git
+if [ ! -f "/usr/bin/git" ]; then
+    sudo apt-get install git screen -y;
+fi
+
+#Installs screen
+if [ ! -f "/usr/bin/screen" ]; then
+    sudo apt-get install screen -y;
+fi
+
+#Updates repo
+git init;
+git remote add origin https://github.com/joaquimguimaraes/chiabot.git;
 git stash; git pull;
+
 dart pub get;
 screen -d -R -S chiabot sh run.sh $1 ;
