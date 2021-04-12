c6a424726c58cfaaac3671b06126164995764004
Joaquim <jgilfnuk@gmail.com>
2021-04-12 18:25:29 +0100

    run.sh now installs git

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 #!bin/sh
 if [ ! -f "/usr/bin/dart" ]; then
 
-    echo "Installing dependencies (dart)";
+    echo "Installing dependencies (dart and git)";
     sleep 2 ;
     wget https://storage.googleapis.com/dart-archive/channels/stable/release/latest/linux_packages/dart_2.12.2-1_amd64.deb -O dart.deb;
     sudo dpkg -i dart.deb;
+    sudo apt-get install git -y;
     sudo apt-get install -f;
     rm dart.deb;
 
