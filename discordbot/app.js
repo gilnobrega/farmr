161d261bf0ea475449c33e30fe3f811fd35ce23e
Joaquim <jgilfnuk@gmail.com>
2021-05-07 20:48:17 +0100

    discord bot now runs chiabot_server binary instead

--- discordbot/app.js
+++ discordbot/app.js
@@ -190,16 +190,16 @@ client.on('message', (msg) => {
     }
     //CHIA RELATED COMMAND
     else if (command === "chia" && args.length == 0) {
-      runCommand("/usr/bin/dart ../server/chiabot_server.dart " + msg.author.id, msg, true);
+      runCommand("../server/chiabot_server.exe " + msg.author.id, msg, true);
     }
     else if (command === "chia" && msg.author.id == "829055244499419178" && args.length == 1) {
-      runCommand("/usr/bin/dart ../server/chiabot_server.dart " + args[0], msg, true);
+      runCommand("../server/chiabot_server.exe " + args[0], msg, true);
     }
     else if (command === "chia" && args.length == 1 && args[0] == "full") {
-      runCommand("/usr/bin/dart ../server/chiabot_server.dart " + msg.author.id + " full", msg, true);
+      runCommand("../server/chiabot_server.exe " + msg.author.id + " full", msg, true);
     }
     else if (command === "chia" && args.length == 1 && args[0] == "workers") {
-      runCommand("/usr/bin/dart ../server/chiabot_server.dart " + msg.author.id + " workers", msg, true);
+      runCommand("../server/chiabot_server.exe " + msg.author.id + " workers", msg, true);
     }
     else if (command === 'chia' && args.length == 1 && args[0] == 'help') {
 
