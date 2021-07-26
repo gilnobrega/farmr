b2fe9046c6dcf8cb62e1c8557282f4504d573f6c
Joaquim Guimaraes <jgilfnuk@gmail.com>
2021-07-26 02:45:10 +0100

    Updates foxyPoolWallet to use foxypool's HTTP api websockets no more!

--- lib/wallets/poolWallets/foxyPoolWallet.dart
+++ lib/wallets/poolWallets/foxyPoolWallet.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:farmr_client/blockchain.dart';
 import 'package:farmr_client/wallets/poolWallets/genericPoolWallet.dart';
 
@@ -5,15 +7,12 @@ import 'dart:async';
 
 import 'package:farmr_client/server/netspace.dart';
 import 'package:logging/logging.dart';
-import 'package:socket_io_client/socket_io_client.dart' as IO;
+import 'package:http/http.dart' as http;
 
 Logger log = Logger("FoxyPool API");
 
 class FoxyPoolWallet extends GenericPoolWallet {
-  final String publicKey;
-  IO.Socket? _socket;
-
-  bool _queryComplete = false;
+  String publicKey;
 
   FoxyPoolWallet(
       {required Blockchain blockchain,
@@ -21,72 +20,39 @@ class FoxyPoolWallet extends GenericPoolWallet {
       String name = "FoxyPool Wallet"})
       : super(blockchain: blockchain, name: name);
 
+  static String _foxyPoolUrl(String poolIdentifier) =>
+      "https://api2.foxypool.io/api/v1/$poolIdentifier/account/";
+
   Future<void> init() async {
     if (publicKey != "") {
-      Stopwatch stopwatch = Stopwatch();
-
-      stopwatch.start();
+      //appends 0x at beginning of public key if it doesnt start with it
+      if (!publicKey.startsWith("0x")) publicKey = "0x$publicKey";
 
       try {
-        _getBalance(publicKey, blockchain);
+        String contents = await http.read(
+            Uri.parse(_foxyPoolUrl("${blockchain.binaryName}-og") + publicKey));
+
+        var data = jsonDecode(contents);
+
+        try {
+          pendingBalance =
+              (double.parse(data['pending']?.toString() ?? "-1.0") *
+                      blockchain.majorToMinorMultiplier)
+                  .round();
+          collateralBalance =
+              (double.parse(data['collateral']?.toString() ?? "-1.0") *
+                      blockchain.majorToMinorMultiplier)
+                  .round();
+          currentPoints = data['shares'] ?? -1;
+          capacity = NetSpace.sizeStringToInt("${data['ec']} GiB").round();
+        } catch (error) {
+          log.warning("Error parsing FoxyPool info!");
+          log.info(error.toString());
+        }
       } catch (e) {
         log.warning(
             "Failed to get FoxyPool Info, make sure your pool public key is correct.");
       }
-
-      //maximum of 5 seconds
-      while (!_queryComplete && stopwatch.elapsedMilliseconds < 5000) {
-        await Future.delayed(Duration(seconds: 1));
-      }
-
-      //print("finished");
-      stopwatch.stop();
-      //print(stopwatch.elapsedMilliseconds);
-
-      //print(this.collateralBalance);
-      //print(this.pendingBalance);
-      //print(this.effectiveCapacity);
-      //print(this.shares);
     }
-  }
-
-  void _getBalance(String poolPublicKey, Blockchain blockchain) {
-    // Dart client
-    _socket = IO.io(
-      'https://api.${blockchain.binaryName}-og.foxypool.io/stats',
-      <String, dynamic>{
-        'transports': ['websocket'],
-      },
-    );
-    _socket?.onConnect((_) {
-      _socket?.emitWithAck('account:fetch', {
-        '${blockchain.binaryName}-og',
-        {'poolPublicKey': poolPublicKey}
-      }, ack: (data) {
-        //print('ack $data');
-        if (data != null) {
-          //print(data);
-          try {
-            pendingBalance = (double.parse(data['pending'].toString()) *
-                    blockchain.majorToMinorMultiplier)
-                .round();
-            collateralBalance = (double.parse(data['collateral'].toString()) *
-                    blockchain.majorToMinorMultiplier)
-                .round();
-            currentPoints = double.parse(data['shares'].toString()).round();
-            capacity = NetSpace.sizeStringToInt("${data['ec']} GiB").round();
-          } catch (error) {
-            log.warning("Error parsing FoxyPool info!");
-            log.info(error.toString());
-          }
-        } else {
-          log.warning(
-              "Failed to get FoxyPool Info, make sure your pool public key is correct.");
-        }
-        _queryComplete = true;
-
-        _socket?.dispose();
-      });
-    });
   }
 }
